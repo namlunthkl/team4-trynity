7a7b4bd509edde7f06851978c88500e7409437de
PSull48 <PSull48@gmail.com>
2012-02-22 01:16:44 +0000

    music and credits

--- PROJECT/source/States/CCreditsState.cpp
+++ PROJECT/source/States/CCreditsState.cpp
@@ -68,11 +68,17 @@ void CCreditsState::Update(float fElapsedTime)
 
 void CCreditsState::Render()
 {
-	pFont->Write("PROGRAMMING", 24, 2, D3DCOLOR_XRGB(255, 255, 255));
-	pFont->Write("Ari Bodaghee", 32, 3, D3DCOLOR_XRGB(255, 255, 255));
-	pFont->Write("Daniel Lima", 32, 4, D3DCOLOR_XRGB(255, 255, 255));
-	pFont->Write("Bryan Schotanes", 32, 5, D3DCOLOR_XRGB(255, 255, 255));
-	pFont->Write("Phil Sullivan", 32, 6, D3DCOLOR_XRGB(255, 255, 255));
-	pFont->Write("MUSIC BY", 24, 8, D3DCOLOR_XRGB(255, 255, 255));
-	pFont->Write("Kim Stennabb Caesar", 32, 9, D3DCOLOR_XRGB(255, 255, 255));
+	pFont->Write("PROGRAMMING BY", 24, 1, D3DCOLOR_XRGB(255, 255, 255));
+	pFont->Write("Ari Bodaghee", 32, 2, D3DCOLOR_XRGB(255, 255, 255));
+	pFont->Write("Daniel Lima", 32, 3, D3DCOLOR_XRGB(255, 255, 255));
+	pFont->Write("Bryan Schotanes", 32, 4, D3DCOLOR_XRGB(255, 255, 255));
+	pFont->Write("Phil Sullivan", 32, 5, D3DCOLOR_XRGB(255, 255, 255));
+	pFont->Write("MUSIC BY", 24, 7, D3DCOLOR_XRGB(255, 255, 255));
+	pFont->Write("Kim Stennabb Caesar", 32, 8, D3DCOLOR_XRGB(255, 255, 255));
+	pFont->Write("LED BY", 24, 10, D3DCOLOR_XRGB(255, 255, 255));
+	pFont->Write("John OLeske", 32, 11, D3DCOLOR_XRGB(255, 255, 255));
+	pFont->Write("Rob Martinez", 32, 12, D3DCOLOR_XRGB(255, 255, 255));
+	pFont->Write("Shawn Paris", 32, 13, D3DCOLOR_XRGB(255, 255, 255));
+	pFont->Write("Sean Hathaway", 32, 14, D3DCOLOR_XRGB(255, 255, 255));
+	pFont->Write("Press Enter", 32, 16, D3DCOLOR_XRGB(255, 255, 255));
 }
