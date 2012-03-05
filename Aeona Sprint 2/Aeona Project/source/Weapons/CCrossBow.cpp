bf96c4b08621651b46f806cdae2a953622b04ceb
Mukian13 <Mukian13@gmail.com>
2012-03-05 05:33:03 +0000

    Different Animations for each weapon

--- Aeona Sprint 2/Aeona Project/source/Weapons/CCrossBow.cpp
+++ Aeona Sprint 2/Aeona Project/source/Weapons/CCrossBow.cpp
@@ -6,28 +6,33 @@
 ////////////////////////////////////////////////////////////////////////
 #include "StdAfx.h"
 #include "CCrossBow.h"
-
+#include "../Game Objects/CPlayer.h"
 CCrossBow::CCrossBow()
 {
+
+	//Load the Animation
+	CBaseCharacter::LoadAnimations("resource/PlayerWCrossBow.xml");
+	SetAttacking(false);
+}
+void CCrossBow::Render(PointD nPos)
+{
+	SetPosX(nPos.x);
+	SetPosY(nPos.y);
+
+	CBaseCharacter::Render();
+}
+
+void CCrossBow::Update(float fElapsedTime)
+{
+	CBaseCharacter::Update(fElapsedTime);
 
 }
 void CCrossBow::Attack(void)
 {
-	//
+	SetAttacking(true);
+
 }
 void CCrossBow::ChargedAttack(void)
 {
-	//
-}
-void CCrossBow::Render(PointD nPosition)
-{
 
 }
-void CCrossBow::Update(float fElapsedTime)
-{
-
-}
-//void CCrossBow::SetWeaponRotation(float fWeaponRotation)
-//{
-//
-//}
--- Aeona Sprint 2/Aeona Project/source/Weapons/CDagger.cpp
+++ Aeona Sprint 2/Aeona Project/source/Weapons/CDagger.cpp
@@ -6,27 +6,33 @@
 ////////////////////////////////////////////////////////////////////////
 #include "StdAfx.h"
 #include "CDagger.h"
+#include "../Game Objects/CPlayer.h"
 CDagger::CDagger()
 {
+
+	//Load the Animation
+	CBaseCharacter::LoadAnimations("resource/PlayerWDagger.xml");
+	SetAttacking(false);
+}
+void CDagger::Render(PointD nPos)
+{
+	SetPosX(nPos.x);
+	SetPosY(nPos.y);
+
+	CBaseCharacter::Render();
+}
+
+void CDagger::Update(float fElapsedTime)
+{
+	CBaseCharacter::Update(fElapsedTime);
 
 }
 void CDagger::Attack(void)
 {
-	//
+	SetAttacking(true);
+
 }
 void CDagger::ChargedAttack(void)
 {
-	//
-}
-void CDagger::Render(PointD nPosition)
-{
 
 }
-void CDagger::Update(float fElapsedTime)
-{
-
-}
-//void CDagger::SetWeaponRotation(float fWeaponRotation)
-//{
-//
-//}
