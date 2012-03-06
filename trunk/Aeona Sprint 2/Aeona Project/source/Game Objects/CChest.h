16c2dc608ebd7776ed255aa4f714ba27cf92a41c
Mukian13@gmail.com <Mukian13@gmail.com@21fa8f09-a018-a166-bbdf-4a3feec60388>
2012-03-06 18:47:16 +0000

    Since im an idiot... Heres the .h and .cpp for chest

---
+++ trunk/Aeona Sprint 2/Aeona Project/source/Game Objects/CChest.h
@@ -0,0 +1,35 @@
+////////////////////////////////////////////////////////////////////////
+//    File Name				:	"CChest.h"
+//
+//    Author Name			:	Bryan Schotanes
+//    Creation Date			:	02/09/12 - 3:34 AM
+//    Purpose				:	CChest
+////////////////////////////////////////////////////////////////////////
+
+// Standard header protection
+#ifndef CCHEST_H_
+#define CCHEST_H_
+
+// Include parent class
+#include "CNPC.h"
+#include "../Items/IBaseItem.h"
+#include "../Particle Engine/ParticleWeapon.h"
+#include "../Weapons/IBaseWeapon.h"
+#include "../CBitmapFont.h"
+#include "../Util/CDialogueList.h"
+
+class CChest : public CNPC
+{
+private:
+	IBaseItem * m_pItem;
+public:
+	CChest(const char* szName = "noname", bool bActiveTalk = false, double dRange = 0, int sndNPC = -1,
+		double dPositionX = 0, double dPositionY = 0, unsigned int uiSpeed = 0U,
+		int nImageID = -1, unsigned int uiWidth = 0U, unsigned int uiHeight = 0U, bool bActive = false,
+		unsigned int uiMaxHealth = 0, unsigned int uiAttackDamage = 0);
+	~CChest();
+	unsigned int GetType(void) const { return TYPE_CHAR_CHEST; }
+protected:
+
+};
+#endif
